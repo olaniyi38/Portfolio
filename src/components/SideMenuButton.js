2f882adccec8bd7b85baf076e0d667835e238f0f
bamidele-alao sodiq <sodiqalao38@gmail.com>
2021-09-20 10:55:10 +0100

    minor chnage

--- src/components/SideMenuButton.js
+++ src/components/SideMenuButton.js
@@ -5,13 +5,9 @@ const SideMenuButton = ({toggleMenu,active}) => {
     return (
         <>
          <IconButton className={`menu-button ${active ? 'active' : ''} `} onClick={toggleMenu}>
-<<<<<<< HEAD
           <div className="line"></div>
           <div className="line"></div>
           <div className="line"></div>
-=======
-           <div className="icon"></div>
->>>>>>> 6e81457d72e99113136f878ef338bede3c204709
          </IconButton>
         </>
     )
